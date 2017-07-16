502525ffbf8726781997fc429045d16e803a85c2
alexpantyukhin <apantykhin@gmail.com>
2017-07-16 20:11:46 +0400

    add init localcontext

--- src/actor/actor.rs
+++ src/actor/actor.rs
@@ -2,6 +2,7 @@ extern crate futures;
 use futures::Future;
 use std::{io, str};
 use std::time::Duration;
+use std::collections::HashMap;
 
 pub trait Actor {
     fn receive(&self, context: Context) -> Future<Item = (), Error = io::Error>;
@@ -22,11 +23,46 @@ impl PID {
     }
 }
 
-pub struct Context<'a> {
-    parent : PID,
-    slf : PID,
-    sender : PID,
-    actor: &'a Actor,
-    receiveTimeout : Duration,
-    children: Vec<PID>
+pub struct LocalContext<'a> {
+    _producer : fn() -> &'a Actor,
+    _receiveMiddleware: fn(context: Context) -> Future<Item=(), Error = io::Error>,
+    _parent: PID
+}
+
+pub trait Context {
+    fn parent(&self) -> &PID;
+    fn slf(&self) -> &PID;
+    fn sender(&self) -> &PID;
+    fn actor(&self) -> &Actor;
+}
+
+
+impl<'a> LocalContext<'a> {
+    fn new(producer: fn() -> &'a Actor, parent: PID, receiveMiddleware: fn(context: Context) -> Future<Item=(), Error = io::Error>) -> LocalContext {
+        LocalContext {
+            _producer: producer,
+            _parent: parent,
+            _receiveMiddleware: receiveMiddleware
+        }
+    }
+}
+
+impl<'a> Context for LocalContext<'a> {
+    fn parent(&self) -> &PID {
+        &self._parent
+    }
+
+    fn slf(&self) -> &PID {
+        //TODO :return self pid
+        &self._parent
+    }
+
+    fn sender(&self) -> &PID {
+        // TODO: return sender pid
+        &self._parent
+    }
+
+    fn actor(&self) -> &Actor {
+        (self._producer)()
+    }
 }
